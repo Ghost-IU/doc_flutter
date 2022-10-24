c8e4e4723b084b9e808d9899ac9f91399e4fca4f
Ghost-IU <projectghost.iu@gmail.com>
2022-10-24 20:10:03 +0530

    Handle colors

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,14 +1,30 @@
+import 'package:doc_flutter/colors.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: Center(
-        child: ElevatedButton.icon(onPressed: (), icon: icon, label: label),
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            "Sign in with Google",
+            style: TextStyle(
+              color: kBlackColor,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            primary: kWhiteColor,
+            minimumSize: const Size(150, 50),
+          ),
+        ),
       ),
     );
   }
