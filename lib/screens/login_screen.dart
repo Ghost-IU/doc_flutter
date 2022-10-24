e36418140f11d6f22c2f6f1ab7d4e17547252f84
Ghost-IU <projectghost.iu@gmail.com>
2022-10-24 19:35:24 +0530

    Initial login screen code
    Add login_screen.dart, import the logos & modify the yaml file

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget{
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(onPressed: (), icon: icon, label: label),
+      ),
+    );
+  }
+}
